6a10f01aacbcbf39370e319842d941230fc1820b
KIRBY-PARK <pkirby0426@gmail.com>
2024-12-24 01:25:17 +0900

    feat(auth):mvc pattern practice tried to apply mvc pattern.

---
+++ src/main/java/com/spring/basic/controller/MvcController.java
@@ -0,0 +1,53 @@
+package com.spring.basic.controller;
+
+import com.spring.basic.domain.Member;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller // MVC 패턴부터는 컨트롤러를 사용함
+@RequestMapping("/membermvc")
+
+public class MvcController {
+    // 임시 회원 저장소
+    private final List<Member> memberList = new ArrayList<>();
+
+    /*
+    * 전체 회원 리스트 페이지 조회 API
+    **/
+    @GetMapping // MVC 부터는 UI를 먼저 생각해야 함
+    public String getMemberListView(Model model) {
+        // 요청
+
+        // 비즈니스 로직 처리
+
+        // 데이터베이스 저장,, 조회...
+
+        model.addAttribute("/membermvc", memberList);
+        return "member/member-list";
+    }
+
+    /*
+     * 회원 생성 페이지 조회 API
+     **/
+    @GetMapping("/new")
+    public String createMemberView() {
+        return "member/member-create";
+    }
+
+    /*
+     * 회원 생성 API
+     * @ModelAttribute : Getter와 Setter를 이용해 자동으로 들어오는 요청을 자바 객체로 맵핑 해줌
+     **/
+    @PostMapping
+    public String createMemberAPI(@ModelAttribute Member member) {
+        memberList.add(member);
+        return "redirect:/membermvc";
+    }
+}
---
+++ src/main/java/com/spring/basic/domain/Member.java
@@ -0,0 +1,41 @@
+package com.spring.basic.domain;
+
+public class Member {
+    // 속성
+    private String name; // 이름
+    private String email; // 이메일
+    private String age; // 나이
+
+
+    // 생성자
+
+
+    // 기능
+    // cmd + n -> getter 클릭 -> 자동생성
+
+    // Getter
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    // Setter
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
